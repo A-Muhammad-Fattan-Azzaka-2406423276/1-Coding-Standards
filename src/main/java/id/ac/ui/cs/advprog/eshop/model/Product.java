f3d187d2fa01028b7a824623158da9ff8909e87a
fattanazzaka <fattanazzaka25@gmail.com>
2026-02-10 06:41:38 +0700

    make class Product in model package

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Getter
+public class Product {
+    private String productId;
+    private String productName;
+    private int productQuantity;
+}
